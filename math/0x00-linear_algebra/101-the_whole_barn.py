56f4e5ac68237f872a25628b013666ce62612e9a
Yonkodhia <yasyasdhia@gmail.com>
2021-12-03 13:38:55 +0100

    add function

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""add matrices"""
+
+
+def add_matrices(mat1, mat2):
+    """Recursively construct a new sum of two matrices"""
+    matrices = []
+    x = len(mat1)
+    y = len(mat2)
+    if x != y:
+        return None
+ for i in mat1:
+  for j in mat2:
+new = add_matrices(i, j)
+  if new is None:
+        return None
+    matrices.append(new)
+    return matrices
